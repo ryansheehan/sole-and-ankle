8db1f7a8d0b48f5144d7bd017a483d70782ae77a
ryan <rsheehan@gmail.com>
2021-11-16 18:52:37 -0600

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,11 +27,19 @@ const Header = () => {
 };
 
 const MainHeader = styled.div`
-  padding: 0 32px;
-  border-bottom: 1px solid ${COLORS.gray[300]};
+  padding: 16px 32px;
+  border-bottom: 1px solid ${COLORS.gray[300]};    
+  display: flex;
+  align-items: center;
+  gap: 32px;
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  display: flex;
+  gap: 48px;
+  margin-left: auto;
+  margin-right: auto;
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
